65aa5b3947fed21b6e7f930ac820535ac06f8389
Larry <pudding850806@gmail.com>
2016-05-18 18:14:40 +0800

    finish basic markdown

--- md2html.js
+++ md2html.js
@@ -23,6 +23,7 @@ var current_mode = NORMAL_MODE;
 
 var md2html = function(str){
 
+
     if(str.match(/^#[^#].*$/)){             // # Hello  ->  <h1> Hello </h1>
 
         str = '<h1>' + str.substring(1) + '</h1>';
@@ -34,6 +35,22 @@ var md2html = function(str){
     } else if(str.match(/^###[^#].*$/)){    // ### Hello  ->  <h3> Hello </h3>
 
         str = '<h3>' + str.substring(3) + '</h3>';
+
+    } else if(str.match(/^!\[\](.+)$/)){    // ![](image.jpg)  ->  <img src="image.jpg">
+
+        str = '<img src="' + str.substring(4, str.length-1) + '">';
+
+    } else if(str.match(/^\[.+\](.+)$/)){   // [Click Me](url)  ->  <a href="url"> Click Me </a>
+
+        var leftBigIndex = str.indexOf('[');
+        var rightBigIndex = str.indexOf(']');
+        var leftSmallIndex = str.indexOf('(');
+        var rightSmallIndex = str.indexOf(')');
+
+        var url = str.substring(leftSmallIndex + 1, rightSmallIndex);
+        var text = str.substring(leftBigIndex + 1, rightBigIndex);
+
+        str = '<a href="' + url + '" style="font-size: 55px"> ' + text + ' </a>';
 
     } else if(str.match(/^-.*$/)){          // - item1  ->  <ul><li> item1 </li>
                                             // - item2  ->      <li> item2 </li>
@@ -47,12 +64,12 @@ var md2html = function(str){
         }
         return '';
 
-    } else if(str.match(/^```.*$/)){
-        
-        if(current_mode == CODE_MODE){
-            current_mode = NORMAL_MODE;
-            return mode_buffer + '</code></pre>';
-        } else {
+    } else if(str.match(/^```.*$/)){                // ```                      
+                                                    // function(){              ->  <pre><code>function(){     
+        if(current_mode == CODE_MODE){              //     var a = 123;         ->  var a = 123;                    
+            current_mode = NORMAL_MODE;             //     console.log('123');  ->  console.log('123');
+            return mode_buffer + '</code></pre>';   // }                        ->  </code></pre>
+        } else {                                    // ```                     
             current_mode = CODE_MODE;
             mode_buffer = '<pre><code>';
             return '';
