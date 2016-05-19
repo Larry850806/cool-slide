1230095e445e74f3060283b4570c4278085d5310
Larry <pudding850806@gmail.com>
2016-05-19 23:10:54 +0800

    update src

--- src/outputHTML/question.js
+++ src/outputHTML/question.js
@@ -1,15 +1,15 @@
 var setAnsAttr = function(allAns, wrongAns, correctAns){
-    allAns.css('background-color', '#00CC66');
+    allAns.css('background-color', '#007799');
     console.log(allAns);
     //$('.ans').css('background-color', '#EED19C');
     wrongAns.click(function(event){
-        wrongAns.css('background-color', 'red');
+        wrongAns.css('background-color', '#FFA488');
         alert('你選了' + $(event.target).text() +'，答錯了');
         allAns.unbind('click');
     });
 
     correctAns.click(function(event){
-        wrongAns.css('background-color', 'red'); 
+        wrongAns.css('background-color', '#FFA488'); 
         alert('你選了' + $(event.target).text() +'，你好聰明.');
         allAns.unbind('click');
     });
